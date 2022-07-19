df0515e37710bc4dae4ad5ac4370a57bbd9c8e55
Louis THOMAS <louisthomas.pro@gmail.com>
2022-07-19 06:21:20 +0200

    test 5

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-const nextConfig = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],
-    // disable: process.env.NODE_ENV === "development",
-  },
+const nextConfig = {
+  // pwa: {
+  //   dest: "public",
+  //   register: true,
+  //   skipWaiting: true,
+  //   runtimeCaching,
+  //   buildExcludes: [/middleware-manifest.json$/],
+  //   // disable: process.env.NODE_ENV === "development",
+  // },
   reactStrictMode: true,
   redirects: async () => {
     return [{ source: "/", destination: "/search", permanent: true }];
@@ -18,6 +18,6 @@ const nextConfig = withPWA({
   images: {
     domains: ["https://inaturalist-open-data.s3.amazonaws.com"],
   },
-});
+};
 
 module.exports = nextConfig;
--- pages/life/[id].tsx
+++ pages/life/[id].tsx
@@ -1,21 +1,13 @@
-import { NoPhotography } from "@mui/icons-material";
 import { ImageList, ImageListItem, Link, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment } from "react";
 import BackButton from "../../components/BackButton";
 import FavoriteButton from "../../components/FavoriteButton";
 import { getLife } from "../../firebase/life.firestore";
 
-const Life: NextPage = () => {
-  const [lifeData, setLifeData] = useState(null);
-  useEffect(() => {
-    getLife("39659").then((res) => {
-      setLifeData(res);
-    });
-  }, []);
-
+const Life: NextPage = ({ lifeData }: any) => {
   const router = useRouter();
   if (router.isFallback) {
     return <div>Loading...</div>;
@@ -23,74 +15,55 @@ const Life: NextPage = () => {
 
   return (
     <Fragment>
-      {lifeData ? (
-        <Fragment>
-          <BackButton />
-          <FavoriteButton lifeId={lifeData.id} />
-          <Typography component="h1">{lifeData.french_common_name}</Typography>
-          <Typography
-            variant="caption"
-            color="text.secondary"
-            sx={{ fontStyle: "italic" }}
-          >
-            {lifeData.scientific_name}
-          </Typography>
-          <Link
-            href={lifeData.wikipedia_url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Wikipedia
-          </Link>
+      <BackButton />
+      <FavoriteButton lifeId={lifeData.id} />
+      <Typography component="h1">{lifeData.french_common_name}</Typography>
+      <Typography
+        variant="caption"
+        color="text.secondary"
+        sx={{ fontStyle: "italic" }}
+      >
+        {lifeData.scientific_name}
+      </Typography>
+      <Link
+        href={lifeData.wikipedia_url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Wikipedia
+      </Link>
 
-          <ImageList cols={3} rowHeight={164}>
-            {lifeData.photos.map((photo: any) => (
-              <ImageListItem key={photo.id}>
-                <Image
-                  loader={() => photo.medium_url}
-                  src="/loading-fish.webp" // not working ?
-                  layout="fill"
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-        </Fragment>
-      ) : (
-        "Loading..."
-      )}
+      <ImageList cols={3} rowHeight={164}>
+        {lifeData.photos.map((photo: any) => (
+          <ImageListItem key={photo.id}>
+            <Image
+              loader={() => photo.medium_url}
+              src="/loading-fish.webp" // not working ?
+              layout="fill"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
     </Fragment>
   );
 };
 
 export default Life;
 
-// export async function getStaticProps({ params }: any) {
-//   let lifeData = null;
-//   try {
-//     lifeData = await getLife(params.id.toString());
-//   } catch (error) {
-//     console.error(error);
-//   }
-//   if (lifeData) {
-//     return { props: { lifeData } };
-//   } else {
-//     return { notFound: true };
-//   }
-// }
+export async function getStaticProps({ params }: any) {
+  let lifeData = null;
+  try {
+    lifeData = await getLife(params.id.toString());
+  } catch (error) {
+    console.error(error);
+  }
+  if (lifeData) {
+    return { props: { lifeData } };
+  } else {
+    return { notFound: true };
+  }
+}
 
-// export async function getStaticPaths() {
-//   return { paths: [] as any[], fallback: true };
-// }
-
-// export async function getServerSideProps(context: any) {
-
-//     let lifeData = null;
-//   try {
-//     lifeData = await getLife("39659");
-//   } catch (error) {
-//     console.error(error);
-//   }
-//   if (lifeData) {
-//     return { props: { lifeData } };
-//   }
-// }
+export async function getStaticPaths() {
+  return { paths: [] as any[], fallback: true };
+}
