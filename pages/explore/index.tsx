8024d7eb6b7f7bb94faeab9bbc1ca7c276657c21
Louis THOMAS <louisthomas.pro@gmail.com>
2022-07-20 00:21:45 +0200

    test 13

--- pages/explore/index.tsx
+++ pages/explore/index.tsx
@@ -1,18 +1,32 @@
 import { Typography } from "@mui/material";
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import { Fragment } from "react";
+import { getLife } from "../../firebase/life.firestore";
+import { ILife } from "../../models/Life";
 
-const Explore: NextPage = () => {
+const Explore: NextPage<{ lifeData: ILife }> = ({ lifeData }) => {
   return (
     <Fragment>
       <Typography variant="h1" component="h1">
         Explore
+        {JSON.stringify(lifeData)}
       </Typography>
       <Typography variant="body1" gutterBottom>
         In construction...
       </Typography>
     </Fragment>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.params!;
+  const lifeData = await getLife(id.toString());
+
+  if (lifeData) {
+    return { props: { lifeData } };
+  } else {
+    return { notFound: true };
+  }
 };
 
 export default Explore;
