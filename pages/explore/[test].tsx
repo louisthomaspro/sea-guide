dc02e4841b116a49c557b0bf8688c23f1d0866dd
Louis THOMAS <louisthomas.pro@gmail.com>
2022-07-20 10:50:01 +0200

    test 14

--- pages/explore/[test].tsx
+++ pages/explore/[test].tsx
@@ -1,5 +1,10 @@
 import { Typography } from "@mui/material";
-import type { GetServerSideProps, NextPage } from "next";
+import type {
+  GetServerSideProps,
+  GetStaticPaths,
+  GetStaticProps,
+  NextPage,
+} from "next";
 import { Fragment } from "react";
 import { getLife } from "../../firebase/life.firestore";
 import { ILife } from "../../models/Life";
@@ -18,14 +23,30 @@ const Explore: NextPage<{ lifeData: ILife }> = ({ lifeData }) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { id } = context.params!;
   const lifeData = await getLife("39659");
+
+  // const lifeData = {
+  //   id: 123,
+  //   french_common_name: "test",
+  //   scientific_name: "test",
+  //   wikipedia_url: "test",
+  //   photos: [] as any[],
+  // };
 
   if (lifeData) {
     return { props: { lifeData } };
   } else {
     return { notFound: true };
   }
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [] as any[],
+    fallback: true,
+  };
 };
 
 export default Explore;
