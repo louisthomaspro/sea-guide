f756cdc115e488c2899f5036f18bcbfdc523d08c
Louis THOMAS <louisthomas.pro@gmail.com>
2022-07-20 16:34:37 +0200

    test 38

--- pages/explore/[id].tsx
+++ pages/explore/[id].tsx
@@ -1,11 +1,13 @@
 import { Typography } from "@mui/material";
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { Fragment } from "react";
+import BackButton from "../../components/BackButton";
 import { Favorite } from "../../components/Favorite";
 
 const Explore: NextPage<{ lifeData: number }> = ({ lifeData }) => {
   return (
     <Fragment>
+      <BackButton />
       <Favorite />
       <Typography variant="h1" component="h1">
         Explore
