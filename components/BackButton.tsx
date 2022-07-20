73ef37b657c26ab46ce1afcee4949db5e206d8f9
Louis THOMAS <louisthomas.pro@gmail.com>
2022-07-20 13:54:58 +0200

    test 17

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,15 +1,16 @@
 import { ChevronLeft } from "@mui/icons-material";
 import { Box, IconButton } from "@mui/material";
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 
 export default function BackButton() {
   // const router = useRouter();
 
   return (
-    <Box>
+    <Fragment>
       <IconButton aria-label="back" onClick={() => console.log("ici")}>
         <ChevronLeft />
       </IconButton>
-    </Box>
+    </Fragment>
   );
 }
