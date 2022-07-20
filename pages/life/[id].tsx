dccb1ff140f307b243dea74e949429d4794709ee
Louis THOMAS <louisthomas.pro@gmail.com>
2022-07-20 10:57:16 +0200

    test 15

--- pages/life/[id].tsx
+++ pages/life/[id].tsx
@@ -21,42 +21,14 @@ const Life: NextPage<{ lifeData: ILife }> = ({ lifeData }) => {
 
   return (
     <Fragment>
-      <BackButton />
-      <FavoriteButton lifeId={lifeData.id} />
-      <Typography component="h1">{lifeData.french_common_name}</Typography>
-      <Typography
-        variant="caption"
-        color="text.secondary"
-        sx={{ fontStyle: "italic" }}
-      >
-        {lifeData.scientific_name}
-      </Typography>
-      <Link
-        href={lifeData.wikipedia_url}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Wikipedia
-      </Link>
-
-      <ImageList cols={3} rowHeight={164}>
-        {lifeData.photos.map((photo: any) => (
-          <ImageListItem key={photo.id}>
-            <Image
-              loader={() => photo.medium_url}
-              src="/loading-fish.webp" // not working ?
-              layout="fill"
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
+      Hello
     </Fragment>
   );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const { test } = context.params!;
-  const lifeData = await getLife(test.toString());
+  const { id } = context.params!;
+  const lifeData = await getLife("39659");
 
   // const lifeData = {
   //   id: 123,
